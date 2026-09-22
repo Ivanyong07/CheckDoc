d31079cfa5da34b6f885417d4fec1ca406468390
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-09-22 09:36:29 +0800

    Update stuff

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://checkdoc.onrender.com";
+const API_BASE = "http://127.0.0.1:8000";
 
 let emails = [];
 let selectedEmail = null;
--- src/app/app.py
+++ src/app/app.py
@@ -22,7 +22,7 @@ inbox = Inbox("data")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://doccheck.vercel.app"],
+    allow_origins=["http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
